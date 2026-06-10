b43c54deaee399a05ccf686ef76038655798283a
c4arl0s <c.santiago.cruz@gmail.com>
2026-06-10 15:40:22 -0600

    Add info about write-your-first-assertion

---
+++ Projects/AssertYourSelf/AssertYourSelfTests/AssertYourSelfTests.swift
@@ -0,0 +1,19 @@
+//
+//  AssertYourSelfTests.swift
+//  AssertYourSelfTests
+//
+//  Created by C4rl0s on 10/06/26.
+//
+
+import Testing
+@testable import AssertYourSelf
+
+struct AssertYourSelfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
